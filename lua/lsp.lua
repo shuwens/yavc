184cb3790060cd4e4cce0932d02e4c00246f5cae
Shuwen Sun <sun.shuw@northeastern.edu>
2023-02-27 10:59:49 -0500

    wip: format

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -69,28 +69,57 @@ capabilities.textDocument.foldingRange = {
     lineFoldingOnly = true
 }
 
-local group = vim.api.nvim_create_augroup("DocumentFormatting", { clear = true })
-local function common_on_attach(client, bufnr)
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+-- DEPRECATED: old format
+-- local group = vim.api.nvim_create_augroup("DocumentFormatting", { clear = true })
+-- local function common_on_attach(client, bufnr)
+--     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+--
+--     -- Enable completion triggered by <c-x><c-o>
+--     -- return M
+--     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+--
+--     -- Get signatures (and _only_ signatures) when in argument lists.
+--     require "lsp_signature".on_attach({ doc_lines = 0, handler_opts = { border = "none" } })
+--
+--     if client.supports_method("textDocument/formatting") then
+--         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--         vim.api.nvim_create_autocmd("BufWritePre", {
+--             group = augroup,
+--             buffer = bufnr,
+--             callback = function()
+--                 vim.lsp.buf.format({ bufnr = bufnr })
+--             end,
+--         })
+--     end
+-- end
 
-    -- Enable completion triggered by <c-x><c-o>
-    -- return M
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+        filter = function(client)
+            -- apply whatever logic you want (in this example, we'll only use null-ls)
+            return client.name == "null-ls"
+        end,
+        bufnr = bufnr,
+    })
+end
 
-    -- Get signatures (and _only_ signatures) when in argument lists.
-    require "lsp_signature".on_attach({ doc_lines = 0, handler_opts = { border = "none" } })
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+-- add to your shared on_attach callback
+local on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
         vim.api.nvim_create_autocmd("BufWritePre", {
             group = augroup,
             buffer = bufnr,
             callback = function()
-                vim.lsp.buf.format({ bufnr = bufnr })
+                lsp_formatting(bufnr)
             end,
         })
     end
 end
+
 
 local path = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
 local words = {}
