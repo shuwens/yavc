1a29b32bfdab189270603a164ef44fd1e4d2e934
Shuwen Sun <sun.shuw@northeastern.edu>
2025-01-03 17:02:33 +0000

    pyenv

--- pyenv_setup.sh
+++ pyenv_setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo apt install -y zlib1g-dev lzma-dev zip unzip npm
+sudo apt install -y zlib1g-dev lzma-dev zip unzip npm luarocks
 sudo apt-get install -y build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
-
 
 PYTHON_VERSION=3.12.0
 
@@ -15,9 +14,7 @@ git clone https://github.com/pyenv/pyenv-virtualenv.git $HOME/.pyenv/plugins/pye
 ~/.pyenv/bin/pyenv install ${PYTHON_VERSION}
 ~/.pyenv/bin/pyenv virtualenv ${PYTHON_VERSION} neovim3
 
-
 # pip3 install neovim pynvim
-
 
 echo "Pyenv and pyenv virtualenv is all set"
 echo "# set -Ux PYENV_ROOT $HOME/.pyenv"
